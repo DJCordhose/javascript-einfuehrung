f1d007ef4b96937d095016cf58408bf8ea92ba07
Oliver Zeigermann <oliver.zeigermann@gmail.com>
2015-12-19 00:34:18 +0100

    typescript-teil komplett

--- code/typescript/features/generics.ts
+++ code/typescript/features/generics.ts
@@ -1,12 +1,24 @@
-function identity<T>(arg: T): T {
+/// <reference path="../node_modules/typescript/lib/lib.es6.d.ts" />
+
+interface HasName {
+  name: string;
+}
+
+function identity<T extends HasName>(arg: T): T {
     return arg;
 }
 
-const olli: string = identity('Olli');
-console.log(olli);
+// return type is inferred to HasName
+const olli = identity({name: 'Olli'});
+console.log(olli.name);
 
-// Error: return type does not match
-// const olli: number = identity('Olli');
+const map: Map<HasName, Number> = new Map();
+const KEY1:HasName = {name: 'KEY1'};
+map.set(KEY1, 42);
 
-// Error: parameter type does not match
-// identity<number>('Olli');
+console.log(map.get(KEY1)); // 42
+console.log(map.has(KEY1)); // true
+console.log(map.size); // 1
+
+map.forEach((value: Number, index: HasName) => console.log(`value=${value}, index=${index.name}`)); 
+// value=42, index=KEY1
