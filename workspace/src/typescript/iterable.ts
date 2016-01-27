d91b11424497ae52d8fc7a8a0c8148ba2ee517e4
Oliver Zeigermann <oliver.zeigermann@gmail.com>
2016-01-27 10:19:21 +0100

    more code

---
+++ workspace/src/typescript/iterable.ts
@@ -0,0 +1,59 @@
+import 'babel-polyfill';
+
+let a: any;
+
+  class Person  {
+    _name: string;
+    constructor(name: string) {
+      this._name = name;
+    }
+
+    get name() {
+      return this._name;
+    }
+  }
+
+ const map = new Map<String, String>();
+  for (const person of map) {
+    console.log(person);
+  }
+ 
+
+  const personIterable = {
+    [Symbol.iterator]() {
+      const persons = [new Person('Olli'), new Person('Oma'), new Person('Opa')];
+      let index = 0;
+      const iterator = {
+        next() {
+          const value = index >= persons.length ? null : persons[index];
+          index++;
+          return { done: value === null, value };
+        }
+      };
+      return iterator;
+    }
+  };
+  
+  for (const person of personIterable) {
+    console.log(person.name);
+  }
+  
+  const persons = [...personIterable];
+  console.log(persons);
+
+
+function* gen() {
+    let index = 0;
+      while (index < 1000) {
+        yield index;
+        index++;
+      }
+  } 
+  
+  const iterator = gen();
+  // const iterator = personIterable[Symbol.iterator]();
+  console.log(iterator.next());
+  console.log(iterator.next());
+  console.log(iterator.next());
+  console.log(iterator.next());
+  
